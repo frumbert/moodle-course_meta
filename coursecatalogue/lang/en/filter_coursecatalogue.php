60aa88de774ed7cfc3f13991504fe175f4e5f077
Tim St.Clair <tim.stclair@gmail.com>
2015-07-08 22:14:26 +1000

    clarified wording about built-in fields

--- coursecatalogue/lang/en/filter_coursecatalogue.php
+++ coursecatalogue/lang/en/filter_coursecatalogue.php
@@ -1,21 +1,25 @@
 <?php
- 
+
 $string['filtername'] = 'Course catalogue';
 $string['configure'] = 'Course-in-catalogue template';
 $string['configure_desc'] = 'Template html to use to draw a course in the catalogue.
 
 	Field names match the shortname definition of a course metadata field.
-	Other values are "courseid" and "categoryid", which represent the
-	course row id, and the category of the metdata field.
+	Some fields are automatically included from the Course table:
+
+	   [courseid]: The database row id of the course
+	   [fullname]: The "Course full name" of the course
+	   [shortname]: The "Course short name" of the course
+	   [idnumber]: The "Course ID number" optional field from the course
 
 	To write a field, put the shortname in square brackets. For instance [columnname].
-	
+
 	To conditionally show a section: [#if columnname] inner code [/if].
 	To conditionally hide a section (opposite of IF): [#not columnname] inner code [/not].
 	Note, can test more than one field using [#if colName1,colName2] inner code [/if colName1,colName2].
-	
+
 	To render a table (grid of td) from semicolon-seperated values: [#table columnname].
-	
+
 	To render an image: [#image columnname].
 
 	To render a file hyperlink: [#link columnname].
